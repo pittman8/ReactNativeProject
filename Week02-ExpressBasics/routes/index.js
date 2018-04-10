4555d51ea5acd9a8d4b0f7819e08729209b4bdff
Hannah Pittman <hannah.pittman@bellevuecollege.edu>
2018-04-10 23:28:44 +0000

    changed port number to 8080 for cloud nine

--- Week02-ExpressBasics/routes/index.js
+++ Week02-ExpressBasics/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Week02-ExpressBasics-Pittman' });
+  res.render('index', { title: 'PROG272-Pittman' });
 });
 
 module.exports = router;
