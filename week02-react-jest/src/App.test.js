244d94d75db81b9bb60822fe4c167361687066ef
Your Name <you@example.com>
2018-04-10 20:21:47 -0700

    working on react project

--- week02-react-jest/src/App.test.js
+++ week02-react-jest/src/App.test.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {configure, shallow} from 'enzyme';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter: new Adapter()});
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Jest Create React Tests', function() {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+
+    it('renders and reads H1 text', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <h2>Welcome to React</h2>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders state of File paragraph after button click', () => {
+        const wrapper = shallow(<App />);
+        const fileSign = <p className="App-intro">file: url-file.js</p>;
+        wrapper.find('#getFile').simulate('click');
+        expect(wrapper.contains(fileSign)).toEqual(true);
+    });
+
 });
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            file: 'unknown',
+            foo: 'waiting'
+        };
+    }
+
+    getFile = () => {
+        console.log('getFile called.');
+        this.setState({file: 'url-file.js'})
+    };
+}
+
+
+
+
+
+
