f735625f18c4032f629b5404337184766d8475bc
Your Name <you@example.com>
2018-04-16 20:46:48 -0700

    edited App.test file

--- week02-react-jest/src/App.test.js
+++ week02-react-jest/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {configure, shallow} from 'enzyme';
-import { App } from './App';
+import App  from './App';
 import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
 
