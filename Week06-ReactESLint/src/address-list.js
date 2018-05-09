90f5b0ab47e8cf279fd6cc0ea39515f5a3d84fb0
Your Name <you@example.com>
2018-05-08 17:58:27 -0700

    added .eslintrc.json and .eslintignore

--- Week06-ReactESLint/src/address-list.js
+++ Week06-ReactESLint/src/address-list.js
@@ -1,27 +1,27 @@
-const unknown = 'unknown';
+const unknown = "unknown";
 const addresses = [
-    {
-        firstName: unknown,
-        lastName: unknown,
-        address: unknown,
-        city: unknown,
-        state: unknown,
-        zip: unknown,
-        phone: unknown,
-        fax: unknown,
-        tollfree: unknown
-    },
-    {
-        firstName: 'Patty',
-        lastName: 'Murray',
-        address: '154 Russell Senate Office Building',
-        city: 'Washington',
-        state: 'D.C.',
-        zip: '20510',
-        phone: '(202) 224-2621',
-        fax: '(202) 224-0238',
-        tollfree: '(866) 481-9186'
-    }
+  {
+    firstName: unknown,
+    lastName: unknown,
+    address: unknown,
+    city: unknown,
+    state: unknown,
+    zip: unknown,
+    phone: unknown,
+    fax: unknown,
+    tollfree: unknown
+  },
+  {
+    firstName: "Patty",
+    lastName: "Murray",
+    address: "154 Russell Senate Office Building",
+    city: "Washington",
+    state: "D.C.",
+    zip: "20510",
+    phone: "(202) 224-2621",
+    fax: "(202) 224-0238",
+    tollfree: "(866) 481-9186"
+  }
 ];
 
 export default addresses;
--- Week06-ReactESLint/src/tests/Address.test.js
+++ Week06-ReactESLint/src/tests/Address.test.js
@@ -1,160 +1,176 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {configure, shallow} from 'enzyme';
-import Address from '../components/Address';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import ReactDOM from "react-dom";
+import { configure, shallow } from "enzyme";
+import Address from "../components/Address";
+import Adapter from "enzyme-adapter-react-16";
 import addresses from "../address-list";
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
-describe('Address tests', function() {
+describe("Address tests", function() {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Address />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<Address/>, div);
-        ReactDOM.unmountComponentAtNode(div);
+  it("renders and displays the default first name", () => {
+    const wrapper = shallow(<Address />);
+    //console.log(wrapper.find('AddressShow').prop('address'));
+    expect(wrapper.find("AddressShow").prop("address").firstName).toEqual(
+      "unknown"
+    );
+  });
+
+  it("renders state of firstName after button click", () => {
+    const wrapper = shallow(<Address addressList={addresses} />);
+    wrapper.instance().setAddress();
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find("AddressShow").prop("address").firstName).toEqual(
+        "Patty"
+      );
     });
+  });
 
-    it('renders and displays the default first name', () => {
-        const wrapper = shallow(<Address/>);
-        //console.log(wrapper.find('AddressShow').prop('address'));
-        expect(wrapper.find('AddressShow').prop('address').firstName).toEqual('unknown');
+  it("renders and displays the default last name", () => {
+    const wrapper = shallow(<Address />);
+    //console.log(wrapper.find('AddressShow').prop('address'));
+    expect(wrapper.find("AddressShow").prop("address").lastName).toEqual(
+      "unknown"
+    );
+  });
+
+  it("renders state of lastName after button click", () => {
+    const wrapper = shallow(<Address addressList={addresses} />);
+    wrapper.instance().setAddress();
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find("AddressShow").prop("address").lastName).toEqual(
+        "Murray"
+      );
     });
+  });
 
-    it('renders state of firstName after button click', () => {
-        const wrapper = shallow(<Address addressList={addresses}/>);
-        wrapper.instance().setAddress();
-        setImmediate(() => {
-            wrapper.update();
-            expect(wrapper.find('AddressShow').prop('address').firstName).toEqual('Patty');
-        });
+  it("renders and displays the default address", () => {
+    const wrapper = shallow(<Address />);
+    //console.log(wrapper.find('AddressShow').prop('address'));
+    expect(wrapper.find("AddressShow").prop("address").address).toEqual(
+      "unknown"
+    );
+  });
 
+  it("renders state of address after button click", () => {
+    const wrapper = shallow(<Address addressList={addresses} />);
+    wrapper.instance().setAddress();
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find("AddressShow").prop("address").address).toEqual(
+        "154 Russell Senate Office Building"
+      );
     });
+  });
 
-    it('renders and displays the default last name', () => {
-        const wrapper = shallow(<Address/>);
-        //console.log(wrapper.find('AddressShow').prop('address'));
-        expect(wrapper.find('AddressShow').prop('address').lastName).toEqual('unknown');
+  it("renders and displays the default city", () => {
+    const wrapper = shallow(<Address />);
+    //console.log(wrapper.find('AddressShow').prop('address'));
+    expect(wrapper.find("AddressShow").prop("address").city).toEqual("unknown");
+  });
+
+  it("renders state of city after button click", () => {
+    const wrapper = shallow(<Address addressList={addresses} />);
+    wrapper.instance().setAddress();
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find("AddressShow").prop("address").city).toEqual(
+        "Washington"
+      );
     });
+  });
 
-    it('renders state of lastName after button click', () => {
-        const wrapper = shallow(<Address addressList={addresses}/>);
-        wrapper.instance().setAddress();
-        setImmediate(() => {
-            wrapper.update();
-            expect(wrapper.find('AddressShow').prop('address').lastName).toEqual('Murray');
-        });
+  it("renders and displays the default state", () => {
+    const wrapper = shallow(<Address />);
+    //console.log(wrapper.find('AddressShow').prop('address'));
+    expect(wrapper.find("AddressShow").prop("address").state).toEqual(
+      "unknown"
+    );
+  });
 
+  it("renders state of state after button click", () => {
+    const wrapper = shallow(<Address addressList={addresses} />);
+    wrapper.instance().setAddress();
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find("AddressShow").prop("address").state).toEqual("D.C.");
     });
+  });
 
-    it('renders and displays the default address', () => {
-        const wrapper = shallow(<Address/>);
-        //console.log(wrapper.find('AddressShow').prop('address'));
-        expect(wrapper.find('AddressShow').prop('address').address).toEqual('unknown');
+  it("renders and displays the default zip", () => {
+    const wrapper = shallow(<Address />);
+    //console.log(wrapper.find('AddressShow').prop('address'));
+    expect(wrapper.find("AddressShow").prop("address").zip).toEqual("unknown");
+  });
+
+  it("renders state of zip after button click", () => {
+    const wrapper = shallow(<Address addressList={addresses} />);
+    wrapper.instance().setAddress();
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find("AddressShow").prop("address").zip).toEqual("20510");
     });
+  });
 
-    it('renders state of address after button click', () => {
-        const wrapper = shallow(<Address addressList={addresses}/>);
-        wrapper.instance().setAddress();
-        setImmediate(() => {
-            wrapper.update();
-            expect(wrapper.find('AddressShow').prop('address').address).toEqual('154 Russell Senate Office Building');
-        });
+  it("renders and displays the default phone", () => {
+    const wrapper = shallow(<Address />);
+    //console.log(wrapper.find('AddressShow').prop('address'));
+    expect(wrapper.find("AddressShow").prop("address").phone).toEqual(
+      "unknown"
+    );
+  });
 
+  it("renders state of phone after button click", () => {
+    const wrapper = shallow(<Address addressList={addresses} />);
+    wrapper.instance().setAddress();
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find("AddressShow").prop("address").phone).toEqual(
+        "(202) 224-2621"
+      );
     });
+  });
 
-    it('renders and displays the default city', () => {
-        const wrapper = shallow(<Address/>);
-        //console.log(wrapper.find('AddressShow').prop('address'));
-        expect(wrapper.find('AddressShow').prop('address').city).toEqual('unknown');
+  it("renders and displays the default fax", () => {
+    const wrapper = shallow(<Address />);
+    //console.log(wrapper.find('AddressShow').prop('address'));
+    expect(wrapper.find("AddressShow").prop("address").fax).toEqual("unknown");
+  });
+
+  it("renders state of fax after button click", () => {
+    const wrapper = shallow(<Address addressList={addresses} />);
+    wrapper.instance().setAddress();
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find("AddressShow").prop("address").fax).toEqual(
+        "(202) 224-0238"
+      );
     });
+  });
 
-    it('renders state of city after button click', () => {
-        const wrapper = shallow(<Address addressList={addresses}/>);
-        wrapper.instance().setAddress();
-        setImmediate(() => {
-            wrapper.update();
-            expect(wrapper.find('AddressShow').prop('address').city).toEqual('Washington');
-        });
+  it("renders and displays the default tollfree", () => {
+    const wrapper = shallow(<Address />);
+    //console.log(wrapper.find('AddressShow').prop('address'));
+    expect(wrapper.find("AddressShow").prop("address").tollfree).toEqual(
+      "unknown"
+    );
+  });
 
+  it("renders state of tollfree after button click", () => {
+    const wrapper = shallow(<Address addressList={addresses} />);
+    wrapper.instance().setAddress();
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find("AddressShow").prop("address").tollfree).toEqual(
+        "(866) 481-9186"
+      );
     });
-
-    it('renders and displays the default state', () => {
-        const wrapper = shallow(<Address/>);
-        //console.log(wrapper.find('AddressShow').prop('address'));
-        expect(wrapper.find('AddressShow').prop('address').state).toEqual('unknown');
-    });
-
-    it('renders state of state after button click', () => {
-        const wrapper = shallow(<Address addressList={addresses}/>);
-        wrapper.instance().setAddress();
-        setImmediate(() => {
-            wrapper.update();
-            expect(wrapper.find('AddressShow').prop('address').state).toEqual('D.C.');
-        });
-
-    });
-
-    it('renders and displays the default zip', () => {
-        const wrapper = shallow(<Address/>);
-        //console.log(wrapper.find('AddressShow').prop('address'));
-        expect(wrapper.find('AddressShow').prop('address').zip).toEqual('unknown');
-    });
-
-    it('renders state of zip after button click', () => {
-        const wrapper = shallow(<Address addressList={addresses}/>);
-        wrapper.instance().setAddress();
-        setImmediate(() => {
-            wrapper.update();
-            expect(wrapper.find('AddressShow').prop('address').zip).toEqual('20510');
-        });
-
-    });
-
-    it('renders and displays the default phone', () => {
-        const wrapper = shallow(<Address/>);
-        //console.log(wrapper.find('AddressShow').prop('address'));
-        expect(wrapper.find('AddressShow').prop('address').phone).toEqual('unknown');
-    });
-
-    it('renders state of phone after button click', () => {
-        const wrapper = shallow(<Address addressList={addresses}/>);
-        wrapper.instance().setAddress();
-        setImmediate(() => {
-            wrapper.update();
-            expect(wrapper.find('AddressShow').prop('address').phone).toEqual('(202) 224-2621');
-        });
-
-    });
-
-    it('renders and displays the default fax', () => {
-        const wrapper = shallow(<Address/>);
-        //console.log(wrapper.find('AddressShow').prop('address'));
-        expect(wrapper.find('AddressShow').prop('address').fax).toEqual('unknown');
-    });
-
-    it('renders state of fax after button click', () => {
-        const wrapper = shallow(<Address addressList={addresses}/>);
-        wrapper.instance().setAddress();
-        setImmediate(() => {
-            wrapper.update();
-            expect(wrapper.find('AddressShow').prop('address').fax).toEqual('(202) 224-0238');
-        });
-
-    });
-
-    it('renders and displays the default tollfree', () => {
-        const wrapper = shallow(<Address/>);
-        //console.log(wrapper.find('AddressShow').prop('address'));
-        expect(wrapper.find('AddressShow').prop('address').tollfree).toEqual('unknown');
-    });
-
-    it('renders state of tollfree after button click', () => {
-        const wrapper = shallow(<Address addressList={addresses}/>);
-        wrapper.instance().setAddress();
-        setImmediate(() => {
-            wrapper.update();
-            expect(wrapper.find('AddressShow').prop('address').tollfree).toEqual('(866) 481-9186');
-        });
-
-    });
+  });
 });
