45381ffb40ad2ffbec92bb6626137c5369accf7e
Your Name <you@example.com>
2018-05-15 14:41:58 -0700

    initial commit

---
+++ AddressNative/Address.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import AddressShow from './AddressShow';
+import addresses from '../address-list';
+
+class Address extends Component {
+    constructor(props) {
+        super(props);
+
+        this.addressIndex = 0;
+        this.state = {
+            address: addresses[this.addressIndex]
+        };
+        this.debug = true;
+    }
+
+    render() {
+        if (!this.debug) {
+            console.log('ADDRESS RENDER');
+        }
+        return (
+            <div className="App">
+                <AddressShow
+                    address={this.state.address}
+                    setAddress={this.setAddress}
+                />
+            </div>
+        );
+    }
+
+    setAddress = () => {
+        this.addressIndex = 1;
+
+        this.setState({
+            address: addresses[this.addressIndex]
+        });
+    };
+}
+
+export default Address;
---
+++ AddressNative/AddressShow.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import ActionAndroid from 'material-ui/svg-icons/action/android';
+import styles from './elf-styles';
+import PropTypes from 'prop-types';
+
+class AddressShow extends Component {
+    constructor(props) {
+        super(props);
+        this.debug = true;
+        if (this.debug) {
+            console.log('SHOW ADDRESS CONSTRUCTOR', this.props.address);
+        }
+    }
+
+    log(message, message2 = '', message3 = '') {
+        if (!this.debug) {
+            const label = this.constructor.name + ': ';
+            console.log(label, message, message2, message3);
+        }
+    }
+
+    render() {
+        this.log('SHOW ADDRESS RENDER', this.props);
+        return (
+            <div className="App">
+                <p className="App-intro">
+                    First Name: {this.props.address.firstName}
+                </p>
+                <p className="App-intro">
+                    Last Name: {this.props.address.lastName}
+                </p>
+                <p className="App-intro">
+                    Address: {this.props.address.address}
+                </p>
+                <p className="App-intro">City: {this.props.address.city}</p>
+                <p className="App-intro">State: {this.props.address.state}</p>
+                <p className="App-intro">Zip: {this.props.address.zip}</p>
+                <p className="App-intro">Phone: {this.props.address.phone}</p>
+                <p className="App-intro">Fax: {this.props.address.fax}</p>
+                <p className="App-intro">
+                    Toll Free: {this.props.address.tollfree}
+                </p>
+                <RaisedButton
+                    label="Set Address"
+                    labelPosition="before"
+                    primary={true}
+                    icon={<ActionAndroid />}
+                    style={styles.button}
+                    onClick={this.props.setAddress}
+                />
+            </div>
+        );
+    }
+}
+
+AddressShow.propTypes = {
+    address: PropTypes.shape({
+        firstName: PropTypes.string,
+        lastName: PropTypes.string,
+        address: PropTypes.string,
+        city: PropTypes.string,
+        state: PropTypes.string,
+        zip: PropTypes.string,
+        phone: PropTypes.string,
+        fax: PropTypes.string,
+        tollfree: PropTypes.string
+    }),
+    setAddress: PropTypes.func
+};
+
+export default AddressShow;
