cc32328b7e30890763dd752dacee1fb45a04b488
Your Name <you@example.com>
2018-05-22 12:48:56 -0700

    re-installed packages

--- AddressMaven/src/index.js
+++ AddressMaven/src/index.js
@@ -15,30 +15,30 @@ ReactDOM.render(
 );
 registerServiceWorker();
 
-router.get('/getAddress', function(request, result) {
-    'use strict';
-    result.send({addresses: [
-        {
-            firstName: unknown,
-            lastName: unknown,
-            address: unknown,
-            city: unknown,
-            state: unknown,
-            zip: unknown,
-            phone: unknown,
-            fax: unknown,
-            tollfree: unknown
-        },
-        {
-            firstName: 'Patty',
-            lastName: 'Murray',
-            address: '154 Russell Senate Office Building',
-            city: 'Washington',
-            state: 'D.C.',
-            zip: '20510',
-            phone: '(202) 224-2621',
-            fax: '(202) 224-0238',
-            tollfree: '(866) 481-9186'
-        }
-    ]});
-});
+// router.get('/getAddress', function(request, result) {
+//     'use strict';
+//     result.send({addresses: [
+//         {
+//             firstName: unknown,
+//             lastName: unknown,
+//             address: unknown,
+//             city: unknown,
+//             state: unknown,
+//             zip: unknown,
+//             phone: unknown,
+//             fax: unknown,
+//             tollfree: unknown
+//         },
+//         {
+//             firstName: 'Patty',
+//             lastName: 'Murray',
+//             address: '154 Russell Senate Office Building',
+//             city: 'Washington',
+//             state: 'D.C.',
+//             zip: '20510',
+//             phone: '(202) 224-2621',
+//             fax: '(202) 224-0238',
+//             tollfree: '(866) 481-9186'
+//         }
+//     ]});
+// });
